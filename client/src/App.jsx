8dc7f215812aa1bbfb3873ec1b75cdc8e099502c
Naren7874 <malinarendra7874@gmail.com>
2025-11-04 16:14:05 +0530

    add nav and layout of the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import AIAgent from "./pages/AIAgent";
+import AnalysisPage from "./pages/AnalysisPage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/ai-agent" element={<AIAgent />} />
+          <Route path="/analysis" element={<AnalysisPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
